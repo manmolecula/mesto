b4ac5741884801c15d143484705ad1f25c7cb11d
Anton Kuchev <kuchevanton@bk.ru>
2023-06-06 12:52:41 +0300

    1 try

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let edit = document.querySelector('.profile__edit-btn');
+let close = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let popupName = document.querySelector('.popup__input_name');
+let subtitle = document.querySelector('.popup__input_subtitle');
+let form = document.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__name');
+let profileSub = document.querySelector('.profile__subtitle');
+function closeForm(){
+    popup.classList.remove('popup_active');
+};
+edit.addEventListener('click', function openForm(){
+    popup.classList.add('popup_active');
+    popupName.value = profileName.textContent;
+    subtitle.value = profileSub.textContent;
+});
+close.addEventListener('click', closeForm);
+form.addEventListener('submit', function editForm(evt){
+    evt.preventDefault();
+    profileName.textContent = popupName.value;
+    profileSub.textContent = subtitle.value;
+    closeForm();
+});
